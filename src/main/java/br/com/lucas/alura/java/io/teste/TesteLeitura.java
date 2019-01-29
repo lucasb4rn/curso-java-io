74050d281d73406b5eb718aa53ba2c30f2c452fa
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-01-29 01:34:54 -0200

    Leitura de um arquivo Lorem.txt

---
+++ src/main/java/br/com/lucas/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,34 @@
+package br.com.lucas.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TesteLeitura {
+
+	public TesteLeitura() {
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("lorem.txt")));
+		
+		/*
+		 * FileInputStream fis = new FileInputStream("lorem.txt");
+		 *  InputStreamReader isr = new InputStreamReader(fis); 
+		 *  BufferedReader br = new BufferedReader(isr);
+		 */
+
+		String linha = br.readLine();
+
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		
+		br.close();
+		
+	}
+
+}
