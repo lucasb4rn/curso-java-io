9e3e548df588a4771cdd45fed8b29b7b8f5a8d94
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-01-31 11:17:16 -0200

    Transformando linha capturada do arquivo em um array de string, agora é possivel capturar o mesmo pelo indice

--- src/main/java/br/com/lucas/alura/java/io/teste/TesteLeitura2Scanner.java
+++ src/main/java/br/com/lucas/alura/java/io/teste/TesteLeitura2Scanner.java
@@ -2,6 +2,7 @@ package br.com.lucas.alura.java.io.teste;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TesteLeitura2Scanner {
@@ -14,7 +15,11 @@ public class TesteLeitura2Scanner {
 		while(scanner.hasNext()){
 		String linha = scanner.nextLine();
 		System.out.println(linha);
-			
+		
+		String[] valores = linha.split(",");
+		System.out.println(valores[0]);
+		
+		
 		}
 		
 		scanner.close();
