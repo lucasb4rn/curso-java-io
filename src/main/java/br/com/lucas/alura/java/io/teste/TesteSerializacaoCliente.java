26505aa5a5850d1565f596ca88da29b32f1891da
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-02-01 00:58:50 -0200

    Escrevendo um objeto Cliente serializado

--- src/main/java/br/com/lucas/alura/java/io/teste/TesteSerializacaoCliente.java
+++ src/main/java/br/com/lucas/alura/java/io/teste/TesteSerializacaoCliente.java
@@ -6,22 +6,25 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-public class TestandoSerializacao {
+public class TesteSerializacaoCliente {
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
-		String nome = "Lucas Henrique Teixeira Fonseca";
+		Cliente cliente = new Cliente();
+		cliente.setNome("Lucas");
+		cliente.setProfissao("Dev");
+		cliente.setCpf("32156465");
 		
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objecto.bin"));
-		oos.writeObject(nome);
+		oos.writeObject(cliente);
 		oos.close();
 		
 		
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objecto.bin"));
-		
-		String nome2 = (String) ois.readObject();
-		
-		System.out.println(nome2);
+		/*
+		 * ObjectInputStream ois = new ObjectInputStream(new
+		 * FileInputStream("objecto.bin")); String nome2 = (String) ois.readObject();
+		 * System.out.println(nome2);
+		 */
 		
 		
 	}
